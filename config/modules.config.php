7678095d94cb50a827ea9c9bf30262dc96df1d4b
LEPTON\mana <tamaoki_mana@yahoo.co.jp>
2018-05-27 18:19:38 +0900

    Set Album Form

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,6 +11,9 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Zend\Form',
+    'Zend\InputFilter',
+    'Zend\Filter',
     'Zend\Db',
     'Zend\Router',
     'Zend\Validator',
--- module/Album/src/Controller/AlbumController.php
+++ module/Album/src/Controller/AlbumController.php
@@ -4,8 +4,8 @@ namespace Album\Controller;
 use Album\Model\AlbumTable;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
-
-
+use Album\Form\AlbumForm;
+use Album\Model\Album;
 
 class AlbumController extends AbstractActionController
 {
@@ -25,6 +25,30 @@ class AlbumController extends AbstractActionController
     
     public function addAction()
     {
+        /* We instantiate AlbumForm and set the label on the submit button to "Add". */ 
+        $form = new AlbumForm();
+        $form->get('submit')->setValue('Add');
+        
+        $request = $this->getRequest();
+        
+        /* If the request is not a POST request, 
+         * then no form data has been submitted and we need to display the form. */
+        if (! $request->isPost()) {
+            return ['form' => $form];
+        }
+        
+        $album = new Album();
+        $form->setInputFilter($album->getInputFilter());
+        $form->setData($request->getPost());
+        
+        /* If form validation fails, we want to redisplay the form. */
+        if (! $form->isValid()) {
+            return ['form' => $form];
+        }
+        
+        $album->exchangeArray($form->getData());
+        $this->table->saveAlbum($album);
+        return $this->redirect()->toRoute('album');
     }
     
     public function editAction()
