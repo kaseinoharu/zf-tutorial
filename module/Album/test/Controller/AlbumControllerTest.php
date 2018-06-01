d3eb42ba6248e0c51634eb3930de12847c8e6c51
LEPTON\mana <tamaoki_mana@yahoo.co.jp>
2018-06-01 23:23:16 +0900

    Test indexAction can be accessed

--- module/Album/test/Controller/AlbumControllerTest.php
+++ module/Album/test/Controller/AlbumControllerTest.php
@@ -8,19 +8,29 @@ use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
 class AlbumControllerTest extends AbstractHttpControllerTestCase
 {
     protected $traceError = false;
-
+    
     public function setUp()
     {
-        /* The module configuration should still be applicable for tests.
-         * You can override configuration here with test case specific values,
-         * such as sample view templates, path stacks, module_listener_options,etc. */
+        /* You can override configuration here with test case specific values. */
         $configOverrides = [];
-
+        
         $this->setApplicationConfig(ArrayUtils::merge(
-            // Grabbing the full application configuration:
-            include __DIR__ . '/../../../../config/application.conf.php',
+            /* Grabbing the full application configration: */
+            include __DIR__. '/../../../../config/application.config.php',
             $configOverrides
         ));
         parent::setUp();
+    }
+    
+    public function testIndexActionCanBeAccessed()
+    {
+        /* Asserts that the response code is 200, and 
+         * ended up in the desired module and controller. */
+        $this->dispatch('/album');
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('Album');
+        $this->assertControllerName(AlbumController::class);
+        $this->assertControllerClass('AlbumController');
+        $this->assertMatchedRouteName('album');
     }
 }
